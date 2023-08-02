c71489cb3fa0352f252665f65f8b5628949327f9
prao3 <44528013+prao3@users.noreply.github.com>
2023-08-01 23:33:36 -0500

    Eval is now number of squares we attack

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -42,6 +42,26 @@ public class MyBot : IChessBot
         A float representing the numerical evaluation of a position
     */
     private float Evaluate(Board board, Move move) {
-        return 0;
+        // If we are white, we want to count black's squares
+        // If we are black, we want to count white's squares
+        // So, in the for loop below, if we are white, we add 32 to the counter, otherwise we add 0
+        int boardSpace = board.IsWhiteToMove ? 32 : 0;
+        // Making the move on the board
+        board.MakeMove(move);
+        // A variable to count the squares we attack
+        int weAttack = 0;
+        // Checking and counting the squares we attack
+        // Only counting squares in the opponent's position
+        for (int i = 0; i < 32; i++) {
+            if (board.SquareIsAttackedByOpponent(new Square(i + boardSpace))) {
+                weAttack++;
+            }
+        }
+        // Undoing our move
+        board.UndoMove(move);
+        // Score of position
+        float Score = weAttack;
+        // Returning score
+        return Score;
     }
 }
