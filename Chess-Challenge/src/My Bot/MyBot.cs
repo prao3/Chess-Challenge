d6decaac6f1cf03e99fbdce196a85b3c238beac1
prao3 <44528013+prao3@users.noreply.github.com>
2023-08-01 21:31:34 -0500

    Making chess bot make random moves

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,18 @@
 ﻿using ChessChallenge.API;
+using System;
+using System.Collections.Generic;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
+        // RNG
+        Random random = new Random();
+        // Legal moves on the board
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        // Random index to pick from
+        int randomIndex = random.Next(moves.Length);
+        // Returning a random move
+        return moves[randomIndex];
     }
 }
