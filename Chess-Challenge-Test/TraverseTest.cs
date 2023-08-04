5c33b7a0319d07fcde5ccd6ac58bd374ef0d843e
prao3 <44528013+prao3@users.noreply.github.com>
2023-08-04 11:59:37 -0500

    Adding test method, not yet implemented

---
+++ Chess-Challenge-Test/TraverseTest.cs
@@ -0,0 +1,16 @@
+namespace Chess_Challenge_Test;
+using ChessChallenge.API;
+using static MyBot;
+
+[TestClass]
+public class TraverseTest {
+    public TraverseTest() {
+
+    }
+
+    [TestMethod]
+    public void TestTraverse() 
+    {
+        Assert.IsTrue(false, "Not Implemented");
+    }
+}
