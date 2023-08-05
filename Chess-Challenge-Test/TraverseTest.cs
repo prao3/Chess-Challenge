f1c862074c4e033bf79097f7513b9791056e092d
prao3 <44528013+prao3@users.noreply.github.com>
2023-08-04 20:04:51 -0500

    Implementing no child test

--- Chess-Challenge-Test/TraverseTest.cs
+++ Chess-Challenge-Test/TraverseTest.cs
@@ -11,16 +11,38 @@ public class TraverseTest {
     private readonly Board KPBoard;
     // Checkmating board
     private readonly Board CheckBoard;
-    
-    public TraverseTest() {
 
+    public TraverseTest() {
+        // Setting up start position
+        StartBoard = Board.CreateBoardFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        // Setting up king and pawn board
+        KPBoard = Board.CreateBoardFromFEN("1k6/p7/8/8/8/8/7P/7K b - - 0 1");
+        //Setting up checkmating board
+        CheckBoard = Board.CreateBoardFromFEN("1k6/2P5/1KP5/1PP5/8/8/8/8 b - - 0 1");
     }
 
     // Testing Traverse when root has no children
     [TestMethod]
     public void RootNoChildrenTest() 
     {
-        Assert.IsTrue(false, "Not Implemented");
+        // Creating move
+        Move StartMove = new Move("e2e4", StartBoard);
+        // Making the move on the board
+        StartBoard.MakeMove(StartMove);
+        // Create root node
+        Node root = new(StartMove, true);
+        // Making the stack
+        Stack<Node> stack = new();
+        // Traversing from root
+        stack = Traverse(root, StartBoard, stack);
+
+        // Testing correctness!!
+        // Getting node on the stack
+        Node StackNode = stack.Pop();
+        // Making sure the node on the stack is the root
+        Assert.AreEqual(root, StackNode);
+        // Making sure the stack is empty
+        Assert.AreEqual(0, stack.Count);
     }
 
     // Testing Traverse for travelling 'deep' in tree
