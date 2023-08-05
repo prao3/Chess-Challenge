0ec69072ea49ca7fc10060f55c92b3d34f734e72
prao3 <44528013+prao3@users.noreply.github.com>
2023-08-04 19:50:38 -0500

    Adding testing methods for Traverse

--- Chess-Challenge-Test/TraverseTest.cs
+++ Chess-Challenge-Test/TraverseTest.cs
@@ -4,12 +4,56 @@ using static MyBot;
 
 [TestClass]
 public class TraverseTest {
+
+    // Board at beginning of a chess game
+    private readonly Board StartBoard;
+    // King and pawn each board
+    private readonly Board KPBoard;
+    // Checkmating board
+    private readonly Board CheckBoard;
+    
     public TraverseTest() {
 
     }
 
+    // Testing Traverse when root has no children
     [TestMethod]
-    public void TestTraverse() 
+    public void RootNoChildrenTest() 
+    {
+        Assert.IsTrue(false, "Not Implemented");
+    }
+
+    // Testing Traverse for travelling 'deep' in tree
+    [TestMethod]
+    public void DeepTraverseTest() 
+    {
+        Assert.IsTrue(false, "Not Implemented");
+    }
+
+    // Testing Traverse for a shallower travel in tree
+    [TestMethod]
+    public void ShallowTraverseTest() 
+    {
+        Assert.IsTrue(false, "Not Implemented");
+    }
+
+    // Testing Traverse for a node not fully populated with children
+    [TestMethod]
+    public void IncompleteNodeTest() 
+    {
+        Assert.IsTrue(false, "Not Implemented");
+    }
+
+    // Testing Traverse for a completely unexplored node
+    [TestMethod]
+    public void UnexploredNodeTest() 
+    {
+        Assert.IsTrue(false, "Not Implemented");
+    }
+
+    // Testing Traverse for decisive result!
+    [TestMethod]
+    public void CheckmateTest() 
     {
         Assert.IsTrue(false, "Not Implemented");
     }
