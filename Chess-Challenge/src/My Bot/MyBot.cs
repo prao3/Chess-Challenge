c2b52ab06242da43284a75e9fcfe01d7105093a9
prao3 <44528013+prao3@users.noreply.github.com>
2023-08-05 09:41:24 -0500

    Fixing UCT calc error (but breaking everything)

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -56,7 +56,7 @@ public class MyBot : IChessBot
         foreach (Node child in children) {
             // Doing UCT
             // TODO: Fix potential divide by zero issues
-            float newScore = child.GetWins() / child.GetVisits() + (float)Sqrt(2*root.GetVisits() / child.GetVisits());
+            float newScore = child.GetWins() / child.GetVisits() + (float)Sqrt(2*Log(root.GetVisits()) / child.GetVisits());
             // If this child is best, save it
             if (newScore > score) {
                 score = newScore;
