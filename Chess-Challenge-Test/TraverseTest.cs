ccf99125a654ed386a1d921b5b5bcf97730be7c4
prao3 <44528013+prao3@users.noreply.github.com>
2023-08-09 13:27:59 -0500

    Implementing MakeTree

--- Chess-Challenge-Test/TraverseTest.cs
+++ Chess-Challenge-Test/TraverseTest.cs
@@ -23,6 +23,7 @@ public class TraverseTest {
 
     /*
     A method to generate a state tree from a given file (for testing only)
+    We assume no repeated moves also
     
     Parameters
     ----------
@@ -32,14 +33,99 @@ public class TraverseTest {
     -------
     The root node of the completed tree
     */
-    private Node MakeTree(string FileName) {
-        return new Node(Move.NullMove, true);
+    private static Node MakeTree(string FileName) {
+        // Reading all text in file
+        string[] AllText = File.ReadAllLines(FileName);
+        // Making a board out of given FEN
+        Board board = Board.CreateBoardFromFEN(AllText[0]);
+        // The next text should be the root, and it should be indexed at 2
+        // The actual info about the root is at index 3
+        string RootText = AllText[3];
+        // Making root node from text
+        Node Root = MakeNodeFromString(RootText, board);
+        // Queue to store all nodes in (except root)
+        Queue<Node> NodesQueue = new();
+        // Adding root node to the queue
+        NodesQueue.Enqueue(Root);
+        // The rest of the info about the tree starts at index 5
+        // We want to loop through all the text and store parent and children info for each set
+        for(int i = 5; i < AllText.Length; i++) {
+            // Making nodes if we are at a line describing a move
+            if (!AllText[i].Contains("parent") && AllText[i] != "") {
+                // Making the current node from string text in file
+                Node CurrentNode = MakeNodeFromString(AllText[i], board);
+                // Storing the current node in a queue
+                NodesQueue.Enqueue(CurrentNode);
+            }
+        }
+        // Converting queue to array
+        Node[] AllNodes = NodesQueue.ToArray();
+        // Node to store current parent
+        Node CurrentParent = Root;
+        // Node to store current child
+        Node CurrentChild = Root;
+        // Looping through text again to assign children to parents
+        for(int i = 5; i < AllText.Length; i++) {
+            // If we are at a parent node, update current parent
+            if(AllText[i].Contains("parent")) {
+                // Getting node info text
+                string NodeText = AllText[i].Remove(0, "parent ".Length);
+                // Making node to use to find parent in nodes
+                Node SearchParent = MakeNodeFromString(NodeText, board);
+                // Searching for current parent and updating variable
+                CurrentParent = Array.Find(AllNodes, node => node.Equals(SearchParent));
+            }
+            // Otherwise, update current child and make it a child of current parent
+            else if(AllText[i] != "") {
+                // Node to search for
+                Node SearchChild = MakeNodeFromString(AllText[i], board);
+                // Searching for current child and updating variable
+                CurrentChild = Array.Find(AllNodes, node => node.Equals(SearchChild));
+                // Assigning child to parent
+                CurrentParent.AddChild(CurrentChild);
+            }
+        }
+        // Returning the root of the tree
+        return Root;
     }
 
-    [TestMethod]
-    public void Test() 
-    {
-        Assert.IsTrue(false);
+    /*
+    A private helper method to make a node from a string
+    The string has format <move> <wins> <visits> <color> (e.g. e2e4 5 20 1)
+    A one in the color spot means white, black is 0
+    */
+    private static Node MakeNodeFromString(string NodeInfo, Board board) {
+        // Splitting string around spaces
+        string[] SubStrings = NodeInfo.Split(' ');
+        // Creating move from substring
+        Move move = new(SubStrings[0], board);
+        // Getting wins and visits
+        float Wins = float.Parse(SubStrings[1]);
+        float Visits = float.Parse(SubStrings[2]);
+        // Getting color
+        bool IsWhite = int.Parse(SubStrings[3]) != 0;
+        // Creating the node
+        Node node = new(move, IsWhite);
+        // Assigning wins and visits to node
+        for (int i = 0; i < Visits; i++) {
+            // If we still need to add wins
+            if (node.GetWins() < Wins) {
+                // If we have a 1/2 a point, we only want to add 0.5
+                if (Wins % 1 != 0) {
+                    node.IncrementWins(0.5f);
+                }
+                // Otherwise, we want to add 1
+                else {
+                    node.IncrementWins(1);
+                }
+            }
+            // Otherwise, we just add 0 to wins counter
+            else {
+                node.IncrementWins(0);
+            }
+        }
+        // Returning our created node
+        return node;
     }
 
     // Testing Traverse when root has no children
