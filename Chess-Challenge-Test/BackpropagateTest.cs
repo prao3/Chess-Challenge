7b8d1919f9bd58de588fcb49fbf7da30b138c93a
prao3 <44528013+prao3@users.noreply.github.com>
2023-08-10 21:41:06 -0500

    Writing test for a draw

--- Chess-Challenge-Test/BackpropagateTest.cs
+++ Chess-Challenge-Test/BackpropagateTest.cs
@@ -4,5 +4,26 @@ using static MyBot;
 
 [TestClass]
 public class BackpropagateTest {
+    [TestMethod]
+    public void DrawTest() {
+        // Test file name
+        string FileName = "..\\..\\..\\BackpropagateTests\\drawtest.txt";
+        // Making tree
+        Node root = TraverseTest.MakeTree(FileName, out Board board);
+        // Traversing tree
+        Stack<Node> stack = new();
+        stack = Traverse(root, board, stack);
+        // Hanging onto nodes
+        Node EndNode = stack.Pop();
+        // Backpropagating
+        Backpropagate(board, stack);
 
+        // Asserting end node visits and wins are correct
+        Assert.AreEqual(2, EndNode.GetVisits());
+        Assert.AreEqual(1, EndNode.GetWins());
+
+        // Checking root visits and wins
+        Assert.AreEqual(11, root.GetVisits());
+        Assert.AreEqual(2, root.GetWins());
+    }
 }
