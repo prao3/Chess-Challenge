c5311947ebfc10b8fa82ba5aecb800175f665b23
prao3 <44528013+prao3@users.noreply.github.com>
2023-08-10 21:43:54 -0500

    Adding a test for white winning

--- Chess-Challenge-Test/BackpropagateTest.cs
+++ Chess-Challenge-Test/BackpropagateTest.cs
@@ -26,4 +26,27 @@ public class BackpropagateTest {
         Assert.AreEqual(11, root.GetVisits());
         Assert.AreEqual(2, root.GetWins());
     }
+
+    [TestMethod]
+    public void WhiteWinTest() {
+        // Test file name
+        string FileName = "..\\..\\..\\BackpropagateTests\\whitewintest.txt";
+        // Making tree
+        Node root = TraverseTest.MakeTree(FileName, out Board board);
+        // Traversing tree
+        Stack<Node> stack = new();
+        stack = Traverse(root, board, stack);
+        // Hanging onto nodes
+        Node EndNode = stack.Pop();
+        // Backpropagating
+        Backpropagate(board, stack);
+
+        // Asserting end node visits and wins are correct
+        Assert.AreEqual(4, EndNode.GetVisits());
+        Assert.AreEqual(4, EndNode.GetWins());
+
+        // Checking root visits and wins
+        Assert.AreEqual(11, root.GetVisits());
+        Assert.AreEqual(3, root.GetWins());
+    }
 }
